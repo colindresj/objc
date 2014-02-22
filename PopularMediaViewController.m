aaf72283ba87cd6d064029f6cd79c66cbdb01d8a
JC <jorge.e.colindres@gmail.com>
2014-02-22 17:32:26 -0500

    create table view with static data

--- PopularMediaViewController.m
+++ PopularMediaViewController.m
@@ -8,7 +8,9 @@
 
 #import "PopularMediaViewController.h"
 
-@interface PopularMediaViewController ()
+@interface PopularMediaViewController () <UITableViewDataSource, UITableViewDelegate>
+
+@property (nonatomic, strong) IBOutlet UITableView *tableView;
 
 @end
 
@@ -26,13 +28,53 @@
 - (void)viewDidLoad
 {
     [super viewDidLoad];
+    
     // Do any additional setup after loading the view from its nib.
+    self.view.backgroundColor = [UIColor whiteColor];
+    self.title = @"WUDUP";
 }
 
 - (void)didReceiveMemoryWarning
 {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
+}
+
+// For navigation/organizational purposes
+#pragma mark - UITableView Delegate
+
+- (void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    UIViewController *viewController = [[UIViewController alloc] init];
+    
+    viewController.view.backgroundColor = [UIColor whiteColor];
+    
+    [self.navigationController pushViewController:viewController animated:YES];
+}
+
+#pragma mark - UITableView Datasource
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    static NSString *cellIdentifier = @"Cell";
+    
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:cellIdentifier];
+    if (!cell)
+    {
+        cell = [[UITableViewCell alloc] initWithStyle:UITableViewCellStyleDefault reuseIdentifier:cellIdentifier];
+    }
+    
+    cell.textLabel.textColor = [UIColor grayColor];
+    cell.backgroundColor = [UIColor whiteColor];
+    cell.textLabel.text = @"I am a row.";
+    
+    return cell;
+    
+}
+
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
+{
+    return 20;
 }
 
 @end
